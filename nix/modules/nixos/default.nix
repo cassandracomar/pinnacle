a8216c1a83842bc51401dc5b58e4ac96b11dc970
Melanie Scheirer <mel@nie.rs>
2025-08-18 01:49:45 -0400

    add UWSM option

--- nix/modules/nixos/default.nix
+++ nix/modules/nixos/default.nix
@@ -4,22 +4,47 @@ let
 in with lib.options; {
   options.programs.pinnacle =  {
     enable = mkEnableOption "pinnacle";
-    xdg-portals.enable = mkEnableOption "xdg-desktop-portal";
-  };
-
-  config = mkIf cfg.enable {
-    services.xserver.displayManager.sessionPackages = [pkgs.pinnacle-server];
-    environment.systemPackages = [pkgs.pinnacle-server];
-
-    services.dbus.enable = true;
-    xdg.portal = mkIf cfg.xdg-portals.enable {
-      enable = true;
-      wlr.enable = true;
-      extraPortals = [
-        pkgs.xdg-desktop-portal-wlr
-        pkgs.xdg-desktop-portal-gtk
-        pkgs.gnome-keyring
-      ];
+    package = mkPackageOption pkgs "pinnacle" {
+      default = "pinnacle";
+      example = "pkgs.pinnacle";
+      extraDescription = "package containing the pinnacle server binary";
     };
+    xdg-portals.enable = mkEnableOption "xdg-desktop-portal";
+    withUWSM = mkEnableOption "uwsm";
   };
+
+  config = mkIf cfg.enable (lib.mkMerge [
+    {
+      environment.systemPackages = [cfg.package];
+      services.dbus.enable = true;
+      xdg.portal = mkIf cfg.xdg-portals.enable {
+        enable = true;
+        wlr.enable = true;
+        extraPortals = [
+          pkgs.xdg-desktop-portal-wlr
+          pkgs.xdg-desktop-portal-gtk
+          pkgs.gnome-keyring
+        ];
+      };
+    }
+    (let
+      pinnacle-session = pkgs.writeShellScript "pinnacle-session" ''
+        #!${pkgs.runtimeShell}
+        exec ${cfg.package}/bin/pinnacle --session
+      '';
+    in lib.mkIf (cfg.withUWSM) {
+      programs.uwsm.enable = true;
+      # Configure UWSM to launch Pinnacle from a display manager like SDDM
+      programs.uwsm.waylandCompositors = {
+        pinnacle = {
+          prettyName = "Pinnacle";
+          comment = "Pinnacle compositor managed by UWSM";
+          binPath = "${pinnacle-session}";
+        };
+      };
+    })
+    (lib.mkIf (!cfg.withUWSM) {
+      services.displayManager.sessionPackages = [cfg.package];
+    })
+  ]);
 }
