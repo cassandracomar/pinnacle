49ecbb1ddc99d3b4c5e354cb1b196ae0469145c4
Cassandra Comar <ccomar@drwholdings.com>
2026-07-27 11:02:22 -0400

    fix: libdisplay-info upgrade

--- nix/packages/default.nix
+++ nix/packages/default.nix
@@ -13,7 +13,7 @@
   mesa,
   xwayland,
   libinput,
-  libdisplay-info,
+  libdisplay-info_0_3,
   git,
   libgbm,
   rustc,
@@ -104,7 +104,7 @@ rustPlatform.buildRustPackage (finalAttrs: {
     libinput
     mesa
     xwayland
-    libdisplay-info
+    libdisplay-info_0_3
     libgbm
     lua5_4
 
--- nix/packages/pinnacle-config.nix
+++ nix/packages/pinnacle-config.nix
@@ -6,26 +6,37 @@
   libxkbcommon,
   libinput,
   lua5_4,
-  libdisplay-info,
+  libdisplay-info_0_3,
   libgbm,
 }:
-{src, ...}@args:
-rustPlatform.buildRustPackage ((builtins.removeAttrs args ["cargoLock" "nativeBuildInputs" "buildInputs"]) // {
-  PINNACLE_PROTOBUF_API_DEFS = ../../api/protobuf;
-  PINNACLE_PROTOBUF_SNOWCAP_API_DEFS = ../../snowcap/api/protobuf;
+{ src, ... }@args:
+rustPlatform.buildRustPackage (
+  (builtins.removeAttrs args [
+    "cargoLock"
+    "nativeBuildInputs"
+    "buildInputs"
+  ])
+  // {
+    PINNACLE_PROTOBUF_API_DEFS = ../../api/protobuf;
+    PINNACLE_PROTOBUF_SNOWCAP_API_DEFS = ../../snowcap/api/protobuf;
 
-  nativeBuildInputs = (args.nativeBuildInputs or []) ++ [protobuf pkg-config];
-  buildInputs = (args.buildInputs or []) ++ [
-    seatd.dev
-    libxkbcommon
-    libinput
-    lua5_4
-    libdisplay-info
-    libgbm
-  ];
+    nativeBuildInputs = (args.nativeBuildInputs or [ ]) ++ [
+      protobuf
+      pkg-config
+    ];
+    buildInputs = (args.buildInputs or [ ]) ++ [
+      seatd.dev
+      libxkbcommon
+      libinput
+      lua5_4
+      libdisplay-info_0_3
+      libgbm
+    ];
 
-  cargoLock = {
-    lockFile = src + /Cargo.lock;
-    allowBuiltinFetchGit = true;
-  } // (args.cargoLock or {});
-})
+    cargoLock = {
+      lockFile = src + /Cargo.lock;
+      allowBuiltinFetchGit = true;
+    }
+    // (args.cargoLock or { });
+  }
+)
